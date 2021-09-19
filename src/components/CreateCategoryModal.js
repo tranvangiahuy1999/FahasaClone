a4367a597ba4d32ed2470a7c91a4d4d553f6edea
Gia Huy <tranvangiahuy1999@gmail.com>
2021-09-19 17:52:44 +0700

    admin cate update

---
+++ src/components/CreateCategoryModal.js
@@ -0,0 +1,156 @@
+import React, { useState, useEffect } from "react";
+import {
+  TextField,
+  Button,
+  FormGroup,
+  Switch,
+  FormControlLabel,
+  makeStyles,
+} from "@material-ui/core";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import AdminApi from "../apis/AdminApis";
+import alert from "../utils/Alert";
+import { LOGO_COLOR, ICON_COLOR } from "../constants/index";
+
+const CreateCategoryModal = (props) => {
+  const classes = useStyles();
+  const [editName, setEditName] = useState();
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    if (props.modalEditFilter) {
+      setEditName(props.modalEditFilter.name);
+      setChecked(props.modalEditFilter.active);
+    }
+  }, []);
+
+  const handleSwitch = () => {
+    setChecked(!checked);
+  };
+
+  const resetModal = () => {
+    setEditName();
+    setChecked(false);
+  };
+
+  const onSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      let formData = {
+        name: editName,
+        parentId: "",
+        active: checked,
+      };
+      let res = null;
+      if (props.modalEditFilter) {
+        res = await AdminApi.updateCategory(
+          props.modalEditFilter._id,
+          formData
+        );
+      } else {
+        res = await AdminApi.createCategory(formData);
+      }
+      if (res.status === 200) {
+        alert({
+          icon: "success",
+          title: res.message,
+          msg: "Cập nhật thành công",
+        });
+      }
+    } catch (e) {
+      console.log(e);
+    }
+    closeModalAfterSave();
+  };
+
+  const closeModalAfterSave = () => {
+    resetModal();
+    props.closeModalAfterSave();
+  };
+
+  const closeModal = () => {
+    resetModal();
+    props.closeModal();
+  };
+
+  return (
+    <Modal
+      aria-labelledby="transition-modal-title"
+      aria-describedby="transition-modal-description"
+      className={classes.modal}
+      open={props.open}
+      onClose={closeModal}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Fade in={props.open}>
+        <div className={classes.paper}>
+          <div className="category-modal">
+            <h5 style={{ color: ICON_COLOR }}>{props.title}</h5>
+            <form onSubmit={onSubmit}>
+              <FormGroup>
+                <TextField
+                  InputProps={{
+                    classes: {
+                      input: classes.labelRoot,
+                    },
+                  }}
+                  label="Tên danh mục"
+                  value={editName}
+                  InputLabelProps={{
+                    classes: {
+                      root: classes.labelRoot,
+                    },
+                  }}
+                  onChange={(e) => setEditName(e.target.value)}
+                  required
+                />
+              </FormGroup>
+              <FormGroup className="mt-3">
+                <FormControlLabel
+                  control={<Switch checked={checked} onChange={handleSwitch} />}
+                  label={<span style={{ fontSize: "1rem" }}>Trạng thái</span>}
+                />
+              </FormGroup>
+              <FormGroup className="mt-3">
+                <Button
+                  variant="contained"
+                  size="small"
+                  style={{ color: "white", backgroundColor: LOGO_COLOR }}
+                  id="material-button-label"
+                  type="submit"
+                >
+                  Lưu
+                </Button>
+              </FormGroup>
+            </form>
+          </div>
+        </div>
+      </Fade>
+    </Modal>
+  );
+};
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(3, 3, 3),
+    borderRadius: "10px",
+  },
+  labelRoot: {
+    fontSize: "1.1rem",
+  },
+}));
+
+export default CreateCategoryModal;
---
+++ src/components/EnhancedTable.js
@@ -0,0 +1,296 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import Button from "@material-ui/core/Button";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableSortLabel from "@material-ui/core/TableSortLabel";
+import Pagination from "@material-ui/lab/Pagination";
+import Paper from "@material-ui/core/Paper";
+import { GoPlus } from "react-icons/go";
+import { IoSearch } from "react-icons/io5";
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Donut", 452, 25.0, 51, 4.9),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Honeycomb", 408, 3.2, 87, 6.5),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Jelly Bean", 375, 0.0, 94, 0.0),
+  createData("KitKat", 518, 26.0, 65, 7.0),
+  createData("Lollipop", 392, 0.2, 98, 0.0),
+  createData("Marshmallow", 318, 0, 81, 2.0),
+  createData("Nougat", 360, 19.0, 9, 37.0),
+  createData("Oreo", 437, 18.0, 63, 4.0),
+];
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+  {
+    id: "name",
+    numeric: false,
+    disablePadding: false,
+    label: "Dessert (100g serving)",
+  },
+  { id: "calories", numeric: true, disablePadding: false, label: "Calories" },
+  { id: "fat", numeric: true, disablePadding: false, label: "Fat (g)" },
+  { id: "carbs", numeric: true, disablePadding: false, label: "Carbs (g)" },
+  { id: "protein", numeric: true, disablePadding: false, label: "Protein (g)" },
+];
+
+function EnhancedTableHead(props) {
+  const { classes, order, orderBy, onRequestSort } = props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <div className="text-center">STT</div>
+        </TableCell>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align="center"
+            padding={headCell.disablePadding ? "none" : "normal"}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <span className={classes.visuallyHidden}>
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  classes: PropTypes.object.isRequired,
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    overflowX: "true",
+  },
+  paper: {
+    width: "100%",
+    marginBottom: theme.spacing(2),
+  },
+  table: {
+    width: "100%",
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: "rect(0 0 0 0)",
+    height: 1,
+    margin: -1,
+    overflow: "hidden",
+    padding: 0,
+    position: "absolute",
+    top: 20,
+    width: 1,
+  },
+}));
+
+export default function EnhancedTable() {
+  const classes = useStyles();
+  const [order, setOrder] = useState("asc");
+  const [orderBy, setOrderBy] = useState("calories");
+  const [selected, setSelected] = useState([]);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = rows.map((n) => n.name);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const isSelected = (name) => selected.indexOf(name) !== -1;
+
+  return (
+    <div className={classes.root}>
+      <div className="row mb-2">
+        <div className="col-lg-6 col-md-6">
+          <h5>Danh sách sản phẩm</h5>
+        </div>
+        <div className="col-lg-6 col-md-6">
+          <div className="row">
+            <div className="col-lg-6 p-2">
+              <TextField
+                id="input-with-icon-textfield"
+                placeholder="Tìm kiếm sản phẩm"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <IoSearch></IoSearch>
+                    </InputAdornment>
+                  ),
+                }}
+                variant="standard"
+              />
+            </div>
+            <div className="col-lg-6 p-2">
+              <Button
+                color="primary"
+                size="small"
+                variant="contained"
+                startIcon={<GoPlus></GoPlus>}
+              >
+                Thêm sản phẩm
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Paper className={classes.paper}>
+        <TableContainer>
+          <Table
+            className={classes.table}
+            aria-labelledby="tableTitle"
+            size="medium"
+            aria-label="enhanced table"
+          >
+            <EnhancedTableHead
+              classes={classes}
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+            />
+            <TableBody>
+              {stableSort(rows, getComparator(order, orderBy)).map(
+                (row, index) => {
+                  const isItemSelected = isSelected(row.name);
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow
+                      hover
+                      onClick={(event) => handleClick(event, row.name)}
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={row.name}
+                      selected={isItemSelected}
+                    >
+                      <TableCell padding="checkbox">
+                        <div className="text-center">{index + 1}</div>
+                      </TableCell>
+                      <TableCell
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                        align="center"
+                      >
+                        {row.name}
+                      </TableCell>
+                      <TableCell align="center">{row.calories}</TableCell>
+                      <TableCell align="center">{row.fat}</TableCell>
+                      <TableCell align="center">{row.carbs}</TableCell>
+                      <TableCell align="center">{row.protein}</TableCell>
+                    </TableRow>
+                  );
+                }
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <div className="p-4 right-wrapper">
+          <Pagination count={10} variant="outlined" shape="rounded" />
+        </div>
+      </Paper>
+    </div>
+  );
+}
