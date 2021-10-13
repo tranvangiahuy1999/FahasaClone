8a11a4d1c25f4090e8c004ee4ba10fb6ee532b66
Nhi4912 <thaonhi8a8@gmail.com>
2021-10-13 20:50:33 +0700

    feat: clean code

--- src/pages/client/Component/ProductDetail.js
+++ src/pages/client/Component/ProductDetail.js
@@ -9,20 +9,8 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
 
 const ProductDetail = () => {
   const [categoryList, setCategoryList] = useState([]);
@@ -502,7 +490,7 @@ const ProductDetail = () => {
 
                       <hr style={{ marginTop: "50px" }} />
                       <div
-                        class="produc-tdescribe-container"
+                        class="product-describe-container"
                         style={{
                           height: "50px",
                         }}
