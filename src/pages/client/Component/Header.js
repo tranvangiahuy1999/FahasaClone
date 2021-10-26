41bc466fe3e7d5e270311f0df5c2ebfeecf2f6f0
Gia Huy <tranvangiahuy1999@gmail.com>
2021-10-26 21:02:40 +0700

    update css

--- src/pages/client/Component/Header.js
+++ src/pages/client/Component/Header.js
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from "react";
 import shopApis from "../../../apis/ShopApis";
-import ProductList from "./ProductList";
+import Carousel from "react-material-ui-carousel";
+import { FcNext, FcPrevious } from "react-icons/fc";
+import { Link } from "react-router-dom";
+
+const imageArray = [
+  "images/banner-sach-moi.jpg",
+  "images/banner-beethoven.jpg",
+  "images/neu-toi-biet-duoc-khi-20-full-banner.jpg",
+];
+
 const Header = () => {
   const [categoryList, setCategoryList] = useState([]);
 
@@ -48,109 +57,77 @@ const Header = () => {
   };
 
   return (
-    <div>
-      {/* thanh tieu de "danh muc sach" + hotline + ho tro truc tuyen */}
-      <section className="duoinavbar">
-        <div
-          className="container  text-white"
-          style={{ background: "white", width: "81%" }}
-        >
-          <div className="row justify">
-            <div className="col-lg-3 md-12" style={{ padding: "0%" }}>
-              <div className="categoryheader" style={{marginLeft:'0%'}}>
-                <div className="noidungheader text-white">
-                  <i
-                    className="fa fa-bars"
-                    id=" menuId"
-                    style={{ marginRight: "10px" }}
-                  />
+    <div className="custom-header mt-3">
+      <div className="custom-header-container">
+        <section>
+          <div className="row m-0 p-0 justify">
+            <div className="col-3 p-0 m-0">
+              <div className="header-title">
+                <div className="text-white">
+                  <i className="fa fa-bars mr-2" id=" menuId" />
                   <span className="text-uppercase font-weight-bold ml-1">
                     Danh mục sách
                   </span>
                 </div>
               </div>
-            </div>
-          </div>
-        </div>
-      </section>
-   
-      {/* noi dung danh muc sach(categories) + banner slider */}
-      <section className="header " style={{ background: "#F0F0F0" }}>
-        <div
-          className="container slidebar"
-          style={{ background: "white", width: "81%" }}
-        >
-          <div className="row">
-            <div className="col-lg-3" style={{ padding: "0%" }}>
-              {/* CATEGORIES */}
-              <div className="categorycontent display">
+              <div className="header-category-list">
                 <ul>
                   {categoryList.length ? (
                     categoryList
                       .filter((item, idx) => idx < 11)
                       .map((value, index) => (
                         <li key={index}>
-                          {" "}
-                          <a
-                            href={
+                          <Link
+                            className="category-item-title one-line-text"
+                            to={
                               "/danh-sach/" +
                               chuyenDoiURL(value.name) +
                               "." +
                               value._id
                             }
-                            className="tieude"
                           >
-                            {" "}
-                            {value.name}
-                          </a>
+                            <span className="subcate-item-0">{value.name}</span>
+                          </Link>
                           <i className="fa fa-chevron-right icon float-right" />
-                          <ul className="categorydetail container">
-                            <div className="row " style={{ height: "185px" }}>
+                          <ul>
+                            <div className="row m-0 p-0">
                               {value.subCate.length ? (
                                 value.subCate
                                   .filter((item, idx) => idx < 6)
                                   .map((value, index) => (
                                     <div className="col-4" key={index}>
-                                      <li className="liheader1">
-                                        <a
-                                          href={
+                                      <li>
+                                        <Link
+                                          to={
                                             "/danh-sach/" +
                                             chuyenDoiURL(value.name) +
                                             "." +
                                             value._id
                                           }
-                                          className="header text-uppercase"
-                                          style={{
-                                            color: "black",
-                                            fontWeight: "600",
-                                          }}
+                                          className="text-uppercase subcate-item-1"
                                         >
-                                          {" "}
-                                          {value.name}
-                                        </a>
+                                          <span className=" one-line-text subcate-1-title">
+                                            {value.name}
+                                          </span>
+                                        </Link>
                                       </li>
-                                      <div
-                                        className="content trai"
-                                        style={{
-                                          marginTop: "-10px",
-                                          marginLeft: "17px",
-                                        }}
-                                      >
+                                      <div className="ml-1 mb-2">
                                         {value.subCate.length ? (
                                           value.subCate
                                             .filter((item, idx) => idx < 5)
                                             .map((value, index) => (
                                               <li key={index}>
-                                                <a
-                                                  href={
+                                                <Link
+                                                  className="subcate-item-2 one-line-text"
+                                                  to={
                                                     "/danh-sach/" +
                                                     chuyenDoiURL(value.name) +
                                                     "." +
                                                     value._id
                                                   }
                                                 >
-                                                  {value.name}
-                                                </a>
+                                                  <span>{value.name}</span>
+                                                </Link>
                                               </li>
                                             ))
                                         ) : (
@@ -172,81 +149,90 @@ const Header = () => {
                 </ul>
               </div>
             </div>
-            {/* banner slider  */}
-            <div className="col-lg-9 md-12 px-0">
-              <div
-                id="carouselId"
-                className="carousel slide"
-                data-ride="carousel"
+            <div className="col-9 p-0 m-0">
+              <Carousel
+                NextIcon={<FcNext className="arrow-icon" />}
+                PrevIcon={<FcPrevious className="arrow-icon" />}
+                indicators={false}
+                fullHeightHover={false}
               >
-                <ol className="nutcarousel carousel-indicators rounded-circle">
-                  <li
-                    data-target="#carouselId"
-                    data-slide-to={0}
-                    className="active"
-                  />
-                  <li data-target="#carouselId" data-slide-to={1} />
-                  <li data-target="#carouselId" data-slide-to={2} />
-                </ol>
-                <div className="carousel-inner" style={{ marginTop: "-40px" }}>
-                  <div className="carousel-item active">
-                    <a href="#">
+                {imageArray.map((ele, i) => (
+                  <div key={i} className="banner-img-wrapper">
+                    <img className="banner-img" alt="" src={ele} />
+                  </div>
+                ))}
+              </Carousel>
+            </div>
+          </div>
+        </section>
+
+        <section className="header-category-list-container">
+          <div>
+            <div className="row m-0 p-0">
+              <div className="col-3 p-0"></div>
+              {/* banner slider  */}
+              <div className="col-9 px-0">
+                {/* <div id="carouselId" className="carousel" data-ride="carousel">
+                  <ol className="nutcarousel carousel-indicators rounded-circle">
+                    <li
+                      data-target="#carouselId"
+                      data-slide-to={0}
+                      className="active"
+                    />
+                    <li data-target="#carouselId" data-slide-to={1} />
+                    <li data-target="#carouselId" data-slide-to={2} />
+                  </ol>
+                  <div className="header-carousel-inner">
+                    <div className="carousel-item active">
                       <img
                         src="images/banner-sach-moi.jpg"
-                        className="img-fluid"
-                        style={{ height: "427px", width: "100%" }}
-                        alt="First slide"
+                        className="header-img-fluid"
+                        alt=""
                       />
-                    </a>
-                  </div>
-                  <div className="carousel-item">
-                    <a href="#">
+                    </div>
+                    <div className="carousel-item">
                       <img
                         src="images/banner-beethoven.jpg"
-                        className="img-fluid"
-                        style={{ height: "427px", width: "100%" }}
-                        alt="Second slide"
+                        className="header-img-fluid"
+                        alt=""
                       />
-                    </a>
-                  </div>
-                  <div className="carousel-item">
-                    <a href="#">
+                    </div>
+                    <div className="carousel-item">
                       <img
                         src="images/neu-toi-biet-duoc-khi-20-full-banner.jpg"
-                        className="img-fluid"
-                        style={{ height: "427px", width: "100%" }}
-                        alt="Third slide"
+                        className="header-img-fluid"
+                        alt=""
                       />
-                    </a>
+                    </div>
                   </div>
-                </div>
-                <a
-                  className="carousel-control-prev"
-                  href="#carouselId"
-                  data-slide="prev"
-                >
-                  <span
-                    className="carousel-control-prev-icon"
-                    aria-hidden="true"
-                  />
-                  <span className="sr-only">Previous</span>
-                </a>
-                <a
-                  className="carousel-control-next"
-                  href="#carouselId"
-                  data-slide="next"
-                >
-                  <span
-                    className="carousel-control-next-icon"
-                    aria-hidden="true"
-                  />
-                  <span className="sr-only">Next</span>
-                </a>
+                  <a
+                    className="carousel-control-prev"
+                    href="#carouselId"
+                    data-slide="prev"
+                  >
+                    <span
+                      className="carousel-control-prev-icon"
+                      aria-hidden="true"
+                    />
+                    <span className="sr-only">Previous</span>
+                  </a>
+                  <a
+                    className="carousel-control-next"
+                    href="#carouselId"
+                    data-slide="next"
+                  >
+                    <span
+                      className="carousel-control-next-icon"
+                      aria-hidden="true"
+                    />
+                    <span className="sr-only">Next</span>
+                  </a>
+                </div> */}
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   );
 };
--- src/pages/client/Component/Nav.js
+++ src/pages/client/Component/Nav.js
@@ -1,158 +1,63 @@
-import React, { useState,useEffect } from "react";
-import Input from "@material-ui/core/Input";
-import shopApis from "../../../apis/ShopApis";
+import React, { useState } from "react";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { Link } from "react-router-dom";
+
 const Nav = () => {
   const [valueProduct, setValueProduct] = useState();
-  const [categoryList, setCategoryList] = useState([]);
-  useEffect(() => {
-    getCategoryData();
-  }, []);
-  const getCategoryData = async () => {
-    try {
-      const res = await shopApis.getCategoryList();
-      console.log(res);
-      if (res.status === 200) {
-        setCategoryList(res.data);
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  const chuyenDoiURL = (str) => {
-    str = str.toLowerCase();
-
-    // xóa dấu
-    str = str.replace(/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/g, "a");
-    str = str.replace(/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/g, "e");
-    str = str.replace(/(ì|í|ị|ỉ|ĩ)/g, "i");
-    str = str.replace(/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/g, "o");
-    str = str.replace(/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/g, "u");
-    str = str.replace(/(ỳ|ý|ỵ|ỷ|ỹ)/g, "y");
-    str = str.replace(/(đ)/g, "d");
-
-    // Xóa ký tự đặc biệt
-    str = str.replace(/([^0-9a-z-\s])/g, "");
-
-    // Xóa khoảng trắng thay bằng ký tự -
-    str = str.replace(/(\s+)/g, "-");
-
-    // xóa phần dự - ở đầu
-    str = str.replace(/^-+/g, "");
-
-    // xóa phần dư - ở cuối
-    str = str.replace(/-+$/g, "");
-
-    // return
-    return str;
-  };
-  const inputChange = (e) => {
-    // prepare value
-    let {
-      target: { name: fieldName, value },
-    } = e;
-    setValueProduct(value);
-    console.log("value", value);
-  };
 
   return (
-    <nav className="navbar navbar-expand-md bg-white navbar-light">
-      <div className="container" style={{ paddingBottom: "0px" }}>
-        <div
-          class="navbar-logo"
-          style={{
-            width: "60px",
-            height: "60",
-            marginLeft: "30px",
-            paddingTop: "0px",
-            marginBottom: "0px",
-          }}
-        >
-          <img
-            style={{ width: "60px", height: "60px", objectFit: "cover" }}
-            src="https://res.cloudinary.com/hanh/image/upload/v1634066828/products/cge2hp4d7bctkrtw1rdj.png"
-            alt=""
-          />
-        </div>
-        <a
-          className="navbar-brand"
-          href="/"
-          style={{ color: "#64ae55", marginLeft: "20px" }}
-        >
-          <b>Nhà Sách KG</b>
-        </a>
-        {/* navbar-toggler  */}
-        <button
-          className="navbar-toggler d-lg-none"
-          type="button"
-          data-toggle="collapse"
-          data-target="#collapsibleNavId"
-          aria-controls="collapsibleNavId"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-          style={{marginTop:'3%'}}
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="collapsibleNavId">
-       
-          {/* form tìm kiếm  */}
-          <form className="form-inline ml-auto my-2 my-lg-0 mr-3">
-              <div className="input-group" style={{width: '630px'}}>
-                <input type="text" className="form-control" aria-label="Small" onChange={inputChange} value={valueProduct} placeholder="Nhập sách cần tìm kiếm..." />
-                {/* <Input    type="text"
-                          name="name"
-                          className="form-control" aria-label="Small"
-                          placeholder="Nhập sách cần tìm kiếm..."
-                          onChange={inputChange}
-                          value={valueProduct}/> */}
+    <nav className="nav">
+      <div className="nav-container">
+        <div className="row m-0 p-0">
+          <div className="col-1">
+            <div className="nav-logo-wrapper">
+              <img
+                className="nav-logo"
+                src="https://res.cloudinary.com/hanh/image/upload/v1634066828/products/cge2hp4d7bctkrtw1rdj.png"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="nav-brand-wrapper col-3 pr-4">
+            <div className="nav-brand-top-text mt-1">Nhà Sách</div>
+            <div className="nav-brand-bot-text">Kiên Giang</div>
+          </div>
+
+          <div className="col-6 nav-search-container">
+            <form className="form-inline mr-3">
+              <div
+                className="input-group nav-search-wrapper"
+                style={{ width: "630px" }}
+              >
+                <input
+                  type="text"
+                  className="form-control nav-search-input"
+                  aria-label="Small"
+                  onChange={(e) => setValueProduct(e.target.value)}
+                  value={valueProduct}
+                  placeholder="Nhập sách cần tìm kiếm..."
+                />
                 <div className="input-group-append">
-                  <a type="button" className="btn" href={"/ket-qua/"+valueProduct} style={{backgroundColor: '#64ae55', color: 'white'}}>
-                    <i className="fa fa-search" />
-                  </a>
-                  {/* <ProductSearchList value={valueProduct}></ProductSearchList> */}
+                  <div
+                    className="search-btn"
+                    style={{ backgroundColor: "#64ae55", color: "white" }}
+                  >
+                    <i className="fa fa-search " />
+                  </div>
                 </div>
               </div>
             </form>
-            <ul className="navbar-nav mb-1 ml-auto">
-          <li className="nav-item giohang">
-          <div className="smallmenu">
-                <input type="checkbox" id="overlay-input" />
-          <label for="overlay-input" id="overlay-button"><span></span></label>
-          <label className="Mobile_name" >Danh Mục Sách</label>
-            <div id="overlay">
-            <ul id="accordion" class="accordion">
-                {categoryList
-                 .filter((item, idx) => idx < 11)
-                 .map((value, index) =>
-                
-                  <li>
-                    <div class="link"><i class="fa fa-database"></i>{value.name}</div>
-                    {/* <ul class="submenu" style={{display:'none'}}>
-                      <li><a href="#">Photoshop</a></li>
-                      <li><a href="#">HTML</a></li>
-                      <li><a href="#">CSS</a></li>
-                    </ul> */}
-                  </li>
-                 )}
-</ul>
-            </div>
-            </div>
-            </li>
-          </ul>
+          </div>
 
-
-          <ul className="navbar-nav mb-1 ml-auto">
-            
-            <li className="nav-item giohang">
-              <a href="/gio-hang" className="btn btn-secondary rounded-circle">
-                <i className="fa fa-shopping-cart" />
-                
-              </a>
-              <a className="nav-link text-dark giohang text-uppercase" href="/gio-hang" style={{display: 'inline-block'}}>Giỏ
-                Hàng</a>
-            </li>
-          </ul>
-         
+          <div className="nav-cart col-2 text-center">
+            <div className="right-wrapper mr-4 mt-3">
+              <AiOutlineShoppingCart
+                size={30}
+                color="orange"
+              ></AiOutlineShoppingCart>
+            </div>
+            <div className="nav-cart-text right-wrapper">GIỎ HÀNG</div>
+          </div>
         </div>
       </div>
     </nav>
